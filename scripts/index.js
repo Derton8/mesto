b842ef2a74fee3266ddbeba70c99f6cfa459f6f6
Danil Mysenko <danil.mysenko@yandex.ru>
2022-10-23 18:46:37 +0300

    hotfix/Вынести переиспользуемый код в отдельные функции + обращение к элементам формы через name

--- scripts/index.js
+++ scripts/index.js
@@ -3,27 +3,35 @@ const POPUP_OPENED_CLASS = 'popup_opened';
 const editBtn = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const closeBtn = popup.querySelector('.popup__close-btn');
-const form = popup.querySelector('.form');
-const nameInput = form.querySelectorAll('.form__input')[0];
-const jobInput = form.querySelectorAll('.form__input')[1];
+const editForm = document.forms.editForm;
+const nickInput = editForm.nick;
+const jobInput = editForm.job;
 const profileName = document.querySelector('.profile__title');
 const profileJob = document.querySelector('.profile__subtitle');
 
-editBtn.addEventListener('click', () => {
+function openPopup() {
   popup.classList.add(POPUP_OPENED_CLASS);
-  nameInput.value = profileName.textContent;
+}
+
+function closePopup() {
+  popup.classList.remove(POPUP_OPENED_CLASS);
+}
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  profileName.textContent = nickInput.value;
+  profileJob.textContent = jobInput.value;
+  closePopup();
+}
+
+editBtn.addEventListener('click', () => {
+  openPopup();
+  nickInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
 });
 
 closeBtn.addEventListener('click', () => {
-  popup.classList.remove(POPUP_OPENED_CLASS);
+  closePopup();
 });
 
-form.addEventListener('submit', (evt) => {
-  evt.preventDefault();
-  let nameValue = nameInput.value;
-  let jobValue = jobInput.value;
-  profileName.textContent = nameValue;
-  profileJob.textContent = jobValue;
-  popup.classList.remove(POPUP_OPENED_CLASS);
-});
+editForm.addEventListener('submit', formSubmitHandler);
